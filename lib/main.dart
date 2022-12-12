d54e6acdee7713e49693fed221e9673f1aa1459d
shanu4182 <shahanazpa04@gmail.com>
2022-12-12 13:49:25 +0530

    list releted commided

--- lib/main.dart
+++ lib/main.dart
@@ -28,28 +28,23 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   
-   List<Icon> scoreKeeper =[
-    Icon(
-              Icons.check,
-              color: Colors.green,
-             ),
-             Icon(
-               Icons.close,
-              color: Colors.red,
-             ),
-             Icon(
-               Icons.close,
-              color: Colors.red,
-             ),
-             Icon(
-               Icons.close,
-              color: Colors.red,
-             ),
-             Icon(
-               Icons.close,
-              color: Colors.red,
-             ),
-        ];
+   List<Icon> scoreKeeper =[];
+
+   List<String>question =[
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+    
+   ];
+   
+   List<bool> answers = [
+    false,
+    true,
+    true,
+   ];
+    
+    int questionNumber = 0 ;
+        
 
   @override
   Widget build(BuildContext context) {
@@ -63,7 +58,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                question[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -89,11 +84,17 @@ class _QuizPageState extends State<QuizPage> {
               
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true) {
+                  print('user got it right!');
+                }else{
+                  print('user got it wrong');
+                }
                 setState(() {
-                scoreKeeper.add(
-                  Icon(Icons.check,color: Colors.red,),
-                );  
-                });
+                    
+                questionNumber++;
+                 });
+                print(questionNumber);
                 },
             ),
           ),
@@ -114,6 +115,17 @@ class _QuizPageState extends State<QuizPage> {
 
               onPressed: () {
                 //The user picked false.
+                 bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == false) {
+                  print('user got it right!');
+                }else{
+                  print('user got it wrong');
+                }
+                setState(() {
+                             
+                questionNumber++;
+                 });
+                 print(questionNumber);
               },
             ),
           ),
@@ -125,6 +137,8 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
+//add first line
+//added
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
